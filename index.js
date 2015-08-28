e0d14424636438958861cdc3cf5502f2c66b5f43
Kyle Mitchell <kyle@kemitchell.com>
2015-08-27 23:14:18 -0700

    Condense code style

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-var sha256 = require('sha256');
-var serialize = require('commonform-serialize');
+var sha256 = require('sha256')
+var serialize = require('commonform-serialize')
 
 module.exports = function(argument) {
-  return sha256(serialize.stringify(argument));
-};
+  return sha256(serialize.stringify(argument)) }

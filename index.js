46321ddb1765270ce9aa91b905330404416610ca
Kyle E. Mitchell <kyle@kemitchell.com>
2016-06-27 16:51:15 -0700

    Use standard style

--- index.js
+++ index.js
@@ -16,5 +16,6 @@
 var sha256 = require('sha256')
 var serialize = require('commonform-serialize')
 
-module.exports = function(argument) {
-  return sha256(serialize.stringify(argument)) }
+module.exports = function (argument) {
+  return sha256(serialize.stringify(argument))
+}

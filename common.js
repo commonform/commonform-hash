4f2a97b22dc5a11c0fbe6d7b8191c9eb319295e0
Kyle E. Mitchell <kyle@kemitchell.com>
2016-07-21 15:42:21 -0700

    Use native crypto on Node.js and sha.js in browsers

---
+++ common.js
@@ -0,0 +1,9 @@
+var serialize = require('commonform-serialize')
+
+module.exports = function (createHash) {
+  return function (argument) {
+    return createHash('sha256')
+    .update(serialize.stringify(argument))
+    .digest('hex')
+  }
+}

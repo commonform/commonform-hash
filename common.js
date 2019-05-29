7a22fbed49a7f41b39087246990ef97ff22c4d04
Kyle E. Mitchell <kyle@kemitchell.com>
2019-05-28 21:36:38 -0700

    npx standard --fix

--- common.js
+++ common.js
@@ -3,7 +3,7 @@ var serialize = require('commonform-serialize')
 module.exports = function (createHash) {
   return function (argument) {
     return createHash('sha256')
-    .update(serialize.stringify(argument))
-    .digest('hex')
+      .update(serialize.stringify(argument))
+      .digest('hex')
   }
 }
